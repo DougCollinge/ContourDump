d3bde29bec6fc5a049534cbafac68bd0bebb3050
Doug Collinge <doug.collinge@gmail.com>
2015-11-07 15:22:46 -0800

    Added udev rule script for linux.

---
+++ bin/enable-usb-bayer.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# TODO Replace this with something a little more delicate and sensitive.
+UDEVRULEPATH=/etc/udev/rules.d/50-BayerContourNextUSB.rules
+echo "# Added to handle the Bayer/Ascenscia Contour Next USB glucometer, a HID device." >$UDEVRULEPATH
+echo "SUBSYSTEM==\"usb\", ATTR\{idVendor\}==\"1a79\", ATTR\{idProduct\}==\"7410\", MODE\:=\"0666\"" >>$UDEVRULEPATH
