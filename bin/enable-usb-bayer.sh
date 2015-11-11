9652e93d10de457d12a1f7465d8f7405990da8fb
djc <doug.collinge@gmail.com>
2015-11-10 23:18:01 -0800

    Should not have escaped the braces.

--- bin/enable-usb-bayer.sh
+++ bin/enable-usb-bayer.sh
@@ -2,4 +2,4 @@
 # TODO Replace this with something a little more delicate and sensitive.
 UDEVRULEPATH=/etc/udev/rules.d/50-BayerContourNextUSB.rules
 echo "# Added to handle the Bayer/Ascenscia Contour Next USB glucometer, a HID device." >$UDEVRULEPATH
-echo "SUBSYSTEM==\"usb\", ATTR\{idVendor\}==\"1a79\", ATTR\{idProduct\}==\"7410\", MODE\:=\"0666\"" >>$UDEVRULEPATH
+echo "SUBSYSTEM==\"usb\", ATTR{idVendor}==\"1a79\", ATTR{idProduct}==\"7410\", MODE:=\"0666\"" >S>$UDEVRULEPATH
